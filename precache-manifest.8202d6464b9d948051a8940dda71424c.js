4882f9c5c12216f28b9cb86b9d48c404186ad29a
DeJa <barclay.deja@gmail.com>
2020-06-26 15:57:35 -0500

    Updates

---
+++ precache-manifest.8202d6464b9d948051a8940dda71424c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee64c74f8f6c80864b65f799395e352",
+    "url": "/React-Todo-List/index.html"
+  },
+  {
+    "revision": "35a6294708e7333c3640",
+    "url": "/React-Todo-List/static/css/main.9cabb9a4.chunk.css"
+  },
+  {
+    "revision": "61e9b55e9a2650879ad0",
+    "url": "/React-Todo-List/static/js/2.a8b7d723.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo-List/static/js/2.a8b7d723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a6294708e7333c3640",
+    "url": "/React-Todo-List/static/js/main.b40cf670.chunk.js"
+  },
+  {
+    "revision": "be26f8c452af6680302d",
+    "url": "/React-Todo-List/static/js/runtime-main.12454da6.js"
+  }
+]);
